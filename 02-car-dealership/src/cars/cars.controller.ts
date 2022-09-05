0ca2e9895f265e7bd3cdee01bef10b9bb7e644fa
GOJAx64 <josealbertogarcia453@gmail.com>
2022-09-04 19:55:59 -0500

    CRUD from scratch

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -14,7 +16,26 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id:string ) {
-        return this.carsService.findOneById( +id );
+    getCarById( @Param('id', ParseUUIDPipe ) id:string ) {
+        console.log({ id });
+        return this.carsService.findOneById( id );
+    }
+
+    @Post()
+    createCar( @Body() createCarDto:CreateCarDto ) { 
+        return this.carsService.create( createCarDto );       
+    }
+
+    @Patch(':id')
+    updateCar( 
+        @Param( 'id', ParseUUIDPipe ) id:string, 
+        @Body() updateCarDto:UpdateCarDto ) 
+    {
+        return this.carsService.update( id, updateCarDto );
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseUUIDPipe ) id:string ) {
+        return this.carsService.delete( id );
     }
 }
